780490e21901892ca10c2b15ab97f6cc96a15370
Allison Howard <alliemhow@gmail.com>
2019-01-02 11:12:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def self.drop_table
+  end
 
 end
